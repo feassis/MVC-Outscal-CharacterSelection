b06b3375a3f6fb123ec9267d677d33ea4771fc00
Felipe Assis <felipe.assis.2002@gmail.com>
2024-09-26 13:40:12 -0300

    Crating tank prefab and tank spawner

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject tankPrefab;
+
+    private void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+}
